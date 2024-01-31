32661a6d180e9a9d30d164f593b67a65951a7589
Rafail Ferre <qa.rafailferre@gmail.com>
2024-01-30 21:03:30 -0500

    created folder "pages", add file LoginDemoQA.ts, add class LoginDemoQA and change loginDemoQA.cy.ts using POM

--- cypress/e2e/loginDemoQA.cy.ts
+++ cypress/e2e/loginDemoQA.cy.ts
@@ -1,16 +1,19 @@
+import { LoginPage } from '../../pages/LoginDemoQA'
+
 describe('Login Page', () => {
     beforeEach(() => {
         cy.visit(`${Cypress.env('urlDemoQA')}/login`)
     })
     it('verify login', () => {
-        cy.get('#userName').type('test')
-        cy.get('#password').type('Test1234*')
-        cy.get('#login').click()
+        LoginPage.submitButtonLogin()
+        // cy.get('#userName').type('test')
+        // cy.get('#password').type('Test1234*')
+        // cy.get('#login').click()
 
         cy.contains('#submit', 'Log out')
         cy.url().should('eq', 'https://demoqa.com/profile')
-        cy.contains('#submit', 'Log out').click()
+        //cy.contains('#submit', 'Log out').click()
 
-        cy.url().should('eq', `${Cypress.env('urlDemoQA')}/login`)
+        //cy.url().should('eq', `${Cypress.env('urlDemoQA')}/login`)
     })
 })
